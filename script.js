0ff77695631e4d020a0c6c7a938b670ac4646ec3
Carl Payne <CulinarytrackerDev@Gmail.com>
2020-08-09 16:31:46 -0400

    finish out project basics

--- script.js
+++ script.js
@@ -1,14 +1,25 @@
 
+const gridContainer = document.querySelector('#grid-container');
+let currentColor = 'black';
 
 function buildGrid(elementType, amtX, amtY) {
-    const gridContainer = document.querySelector('#grid-container');
+    
     const biggest=Math.max(amtX, amtY);
-    gridContainer.style.width=`${(1000-(1000%biggest)*.1)}vmin`;
-    gridContainer.style.height=`${(1000-(1000%biggest)*.1)}vmin`;
+    console.log(`The Largest Dimension is ${biggest} squares.`);
+    const dimension=((1000-(1000%biggest))*.1)-10;
+    console.log(`The best percentage is ${dimension} percent.`);
+    const dimVmin=`${dimension}vmin`;
+    console.log(`The chosen dimension is ${biggest} square.`);
+    const squareSize=dimension/biggest;
+    const squareSizeVmin=`${squareSize}vmin`;
+    gridContainer.style.width=dimVmin;
+    gridContainer.style.height=dimVmin;
     for (let i = 0; i<amtY; i++){
         let newRow = document.createElement('div');
         newRow.className='row';
         newRow.id=('row-'+i);
+        newRow.style.width=dimVmin;
+        newRow.style.height=squareSizeVmin;
        
         
 
@@ -17,9 +28,10 @@ function buildGrid(elementType, amtX, amtY) {
             newElement.classList.add('square');
             newElement.setAttribute('data-row', i);
             newElement.setAttribute('data-col', j);
-            newElement.style.width = (80/amtX + 'vmin');
-            newElement.style.height = (80/amtY + 'vmin');
-            colorSet(newElement);
+            newElement.style.width = squareSizeVmin;
+            newElement.style.height = squareSizeVmin;
+            newElement.style.backgroundColor="white";
+            newElement.addEventListener("mouseover", setColor);
             newRow.appendChild(newElement);
 
         }
@@ -30,17 +42,42 @@ function buildGrid(elementType, amtX, amtY) {
 
 buildGrid('div', 16, 16);
 
+const resetBtn = document.querySelector('#reset');
+resetBtn.addEventListener("click", resetGrid);
 
-function colorSet(a) {
-    let color;
-    if (isSame(a)) {
-        color='red';
-    }else color='blue';
-    a.style.backgroundColor=color;
+const gridSizeBtn = document.querySelector("#gridSizeSubmit");
+gridSizeBtn.addEventListener("click", submitGridSize);
+const drawColorBtn = document.querySelector("#drawColorSubmit");
+drawColorBtn.addEventListener("click", submitDrawColor);
+
+function submitGridSize(){
+    removeGrid();
+    const newSize = document.querySelector("#gridSize").value;
+    buildGrid('div', newSize, newSize);
 }
 
-function isSame(a){
-    const numRow = a.getAttribute('data-row');
-    const numCol = a.getAttribute('data-col');
-    return ((numRow%2)==(numCol%2));
+function submitDrawColor(){
+    const newColor = document.querySelector("#drawColor").value;
+    currentColor=newColor;
 }
+
+function removeGrid() {
+    let rows = Array.from(gridContainer.children);
+    for (let row of rows) {
+        gridContainer.removeChild(row);
+    }
+}
+
+
+function resetGrid(e){
+    let squares = Array.from(document.querySelectorAll('.square'));
+    for (let square of squares) {
+        square.style.backgroundColor="white";
+    }
+}
+
+function setColor(e) {
+    e.target.style.backgroundColor=currentColor;
+    console.log(currentColor);
+}
+
